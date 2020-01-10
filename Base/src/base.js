1aeffefc285f6421ebf7dc5abd03acd00b0f9c60
stuff7 <cuentafrst@gmail.com>
2020-01-10 17:03:19 -0600

    Backup

--- Base/src/base.js
+++ Base/src/base.js
@@ -10,31 +10,30 @@ $onready(function() {
 		home: $("#nav-home"),
 		user: $("#nav-user"),
 		userDropdown: $("#nav-user ul"),
-		colorDropdown: $("#nav-user-color-inner"),
 		langList: $$("#nav-user-lang-inner .scroll input")
 	}
+	setupColorChanges()
 	for(const option of nav.langList) {
 		option.nextSibling.onclick = function() {
 			option.checked||post("user_settings", {language: this.dataset.name})
 				.then(r=> location.reload()).catch(e=> console.log(e))
 		}
 	}
-	const colorPicker = ColorPicker(nav.colorDropdown.$(".color-picker"))
-	const palette = Palette(colorPicker.color.setOptions({min: 255, max: 500})).update().updateHTML()
-	nav.colorDropdown.on("customFocus", ()=> colorPicker.updateUI())
-
-	const randomColorInput = nav.colorDropdown.$(".random-color label input")
-	randomColorInput.on("change", function() {
-		post("user_settings", {site_color: this.checked? "random" : colorPicker.getColorString()})
-	})
-	colorPicker.on("colorchange", ()=> {
-		palette.update().updateHTML()
-		post("user_settings", {site_color: randomColorInput.checked? "random" : colorPicker.getColorString()})
-	})
-
 	for(const dropdown of $$(".dropdown"))
 		Dropdown(dropdown)
 	nav.user.$(".dropdown").on("customBlur", function() {
-		this.$("ul").replaceWith(nav.userDropdown)
+		const ul = this.$("ul")
+		ul.classList.contains("inner-dropdown")&&ul.replaceWith(nav.userDropdown)
 	})
 })
+
+function setupColorChanges() {
+	const dropdown = $("#nav-user-color-inner")
+	const colorPicker = ColorPicker(dropdown.$(".color-picker"))
+	const palette = Palette(colorPicker.color.setOptions({min: 255, max: 500})).update().updateHTML()
+
+	const alwaysRandom = dropdown.$(".random-color label input")
+	colorPicker.on("colorinput", ()=> palette.update().updateHTML())
+	colorPicker.on("colorchange", ()=> post("user_settings",
+		{site_color: alwaysRandom.checked? "random" : colorPicker.getColorString()}))
+}
